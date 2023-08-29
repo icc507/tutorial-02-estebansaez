b0a63feb43842bd692886ae2deebf7f8a64e1d47
estebanSaez <85312860+estebansaez@users.noreply.github.com>
2023-08-29 10:33:51 -0400

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -7,5 +7,11 @@
 #         20 90 hola jiji 77
 #La salida debe ser
 #         (77, 'jiji', 'hola', 90, 20)
-t = input()
-print(t)
+t = input().split()
+
+for i in range(len(t)):
+    if t[i].isdigit():
+        t[i] = int(t[i])
+elementos_invertidos = list(reversed(t))
+
+print(tuple(elementos_invertidos))
