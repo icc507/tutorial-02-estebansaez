1e9a5562702ef52331e3450fdbbf62495edd7cd9
estebanSaez <85312860+estebansaez@users.noreply.github.com>
2023-08-29 10:29:39 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -9,6 +9,15 @@
 #		  mundo 44
 #La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = input()
-m = input()
-print(m)
+t = input().split()
+m = input().split()
+
+for i in range(len(t)):
+    if t[i].isdigit():
+        t[i] = int(t[i])
+
+for i in range(len(m)):
+    if m[i].isdigit():
+        m[i] = int(m[i])
+
+print(tuple(m) + tuple(t) + tuple(m))
